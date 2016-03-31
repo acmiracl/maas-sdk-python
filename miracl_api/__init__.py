1f818832213f8b3aff830bed79b6c091f847fcf7
Elviss Kuštans <n3o59hf@gmail.com>
2016-04-01 00:14:33 +0300

    Expose MiraclException from module

--- miracl_api/__init__.py
+++ miracl_api/__init__.py
@@ -2,3 +2,4 @@ from .api import MiraclClient
 from .api import SESSION_MIRACL_NONCE_KEY
 from .api import SESSION_MIRACL_STATE_KEY
 from .api import SESSION_MIRACL_TOKEN_KEY
+from .api import MiraclException
